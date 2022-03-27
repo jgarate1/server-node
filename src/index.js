4080040620940036ba8595a5cbf152c209a72a44
jgarate1 <garatejustus@gmail.com>
2022-03-27 17:31:15 -0500

    Error Handler Added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import cors from 'cors'
 
 import config from './utils/config'
 import logger from './utils/logger'
+import errors from './utils/errors'
 
 const app = express()
 
@@ -21,4 +22,6 @@ app.get('/', (req, res) => {
   res.send({ msg: 'Yo' })
 })
 
+app.use(errors.notFound)
+app.use(errors.errorHandler)
 app.listen(config.port)
