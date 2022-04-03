496ebaac178441393eff12ae4c85f61038565190
jgarate1 <garatejustus@gmail.com>
2022-04-03 00:02:00 -0500

    final code

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,10 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Router } from 'express'
 
+import auth from '../../utils/auth'
+
 import todos from './todos'
 
 const router = Router()
 
+router.use(auth.authenticate('local', { session: false }))
 router.use('/todos', todos)
 
 export default router
